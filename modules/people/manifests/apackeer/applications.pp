5b542eeb2cc2a22111cc7887e63eb0b2c5683335
Arden Packeer <contactme@ardenpackeer.com>
2013-05-22 15:41:17 +1000

    Sublime Settings

--- modules/people/manifests/apackeer/applications.pp
+++ modules/people/manifests/apackeer/applications.pp
@@ -17,9 +17,9 @@ class people::apackeer::applications {
   include autojump
 
 
-  ############
-  # Packages #
-  ############
+  #####################
+  # Homebrew Packages #
+  #####################
 
   package {
     [
@@ -42,6 +42,10 @@ class people::apackeer::applications {
     command     => 'pip install virtualenv virtualenvwrapper',
     require => Class['python']
   }
+
+  #####################
+  # External Packages #
+  #####################
 
   package { 'Bartender':
     source   => 'http://www.macbartender.com/Demo/Bartender.zip',
@@ -72,4 +76,53 @@ class people::apackeer::applications {
     source   => 'http://get.cocoatech.com/PF6_LION.zip',
     provider => compressed_app,
   }
+
+  ########################
+  # Sublime Text 2 Setup #
+  ########################
+
+  sublime_text_2::package { 'Cucumber':
+    source => 'drewda/cucumber-sublime2-bundle'
+  }
+
+  sublime_text_2::package { 'Git':
+    source => 'kemayo/sublime-text-2-git'
+  }
+
+  sublime_text_2::package { 'GitGutter':
+    source => 'jisaacks/GitGutter'
+  }
+
+  sublime_text_2::package { 'Pretty JSON':
+    source => 'dzhibas/SublimePrettyJson'
+  }
+
+  sublime_text_2::package { 'Puppet':
+    source => 'russCloak/SublimePuppet'
+  }
+
+  sublime_text_2::package { 'SideBarEnhancements':
+    source => 'titoBouzout/SideBarEnhancements'
+  }
+
+  sublime_text_2::package { 'Solarized Color Scheme':
+    source => 'SublimeColors/Solarized'
+  }
+
+  sublime_text_2::package { 'SublimeLinter':
+    source => 'SublimeLinter/SublimeLinter'
+  }
+
+    sublime_text_2::package { 'Theme - Soda':
+    source => 'buymeasoda/soda-theme/'
+  }
+
+  #################
+  # Vagrant Setup #
+  #################
+
+  #vagrant::box { 'precise64/vmware_fusion':
+  #  source => 'http://files.vagrantup.com/precise64_vmware.box'
+  #}
+
 }
